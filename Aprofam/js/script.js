f209fe07dc0704ab07bbc7c8bfedad9669dea09f
CristianMendozaH <cristian8mendoza8@gmail.com>
2024-06-17 15:59:31 -0600

    Ultimas actualizaciones

--- Aprofam/js/script.js
+++ Aprofam/js/script.js
@@ -3,6 +3,7 @@ const navbarShowBtn = document.querySelector('.navbar-show-btn');
 const navbarCollapseDiv = document.querySelector('.navbar-collapse');
 const navbarHideBtn = document.querySelector('.navbar-hide-btn');
 
+
 navbarShowBtn.addEventListener('click', function(){
     navbarCollapseDiv.classList.add('navbar-show');
 });
